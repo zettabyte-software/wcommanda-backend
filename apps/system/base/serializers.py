b36417f472fefca9e82626bf3aea7e20cb289663
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2024-11-27 18:11:21 -0300

    fix: comentando o código que tava dando pau

--- apps/system/base/serializers.py
+++ apps/system/base/serializers.py
@@ -9,14 +9,15 @@ class BaseModelSerializer(serializers.ModelSerializer):
     def __init__(self, instance=None, data=serializers.empty, **kwargs):
         exclude_fields = kwargs.pop("exclude_fields", None)
 
+        # request = self.context.get("request", None)
+        # if request and request.method == "GET":
+        #     self.Meta.read_only_fields = self.Meta.model.get_column_names()  # type: ignore
+
         super().__init__(instance, data, **kwargs)
 
         if exclude_fields is not None:
             for field_name in exclude_fields:
                 self.fields.pop(field_name, None)
-
-        if self.context["request"].method == "GET":
-            self.Meta.read_only_fields = self.Meta.model.get_column_names()  # type: ignore
 
     def get_field_verbose_name(self, field_name):
         return self.Meta.model._meta.get_field(field_name).verbose_name  # type: ignore
