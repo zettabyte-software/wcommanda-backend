f74963ab27ff3039cd4a6126f411d996cace5cd1
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2024-11-27 15:25:19 -0300

    fix: cast do campo para trazer o maior código

--- apps/produtos/services.py
+++ apps/produtos/services.py
@@ -1,8 +1,12 @@
-from django.db.models import Max
+import logging
+
+from django.db.models import IntegerField, Max
+from django.db.models.functions import Cast
 
 from .models import Produto
 
+logger = logging.getLogger(__name__)
 
 def gerar_codigo_cardapio():
-    codigo = Produto.objects.aggregate(codigo=Max("pr_codigo_cardapio"))["codigo"] or 0
+    codigo = Produto.objects.annotate(codigo_num=Cast('pr_codigo_cardapio', IntegerField())).aggregate(codigo=Max('codigo_num'))['codigo'] or 0
     return int(codigo) + 1
