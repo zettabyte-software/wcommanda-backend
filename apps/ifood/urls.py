433b29dd43f463429de369521a1051e47ed70a3a
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2024-11-27 15:10:48 -0300

    feat: importação dos produtos via iFood

--- apps/ifood/urls.py
+++ apps/ifood/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 from .views import IfoodViewSet
 
 router_v1 = DefaultRouter()
-router_v1.register(r'ifood', IfoodViewSet, basename='ifood')
+router_v1.register('ifood', IfoodViewSet, 'ifood')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
--- apps/ifood/views.py
+++ apps/ifood/views.py
@@ -7,7 +7,7 @@ from apps.system.conf.models import Configuracao
 from apps.system.core.classes import DinamicAttrs
 
 from .dataclasses import PedidoIfood
-from .integrators import IntegradorPedidosIfood
+from .integrators import ImportarProdutosIfood, IntegradorPedidosIfood
 
 
 class IfoodViewSet(ViewSet):
@@ -24,3 +24,17 @@ class IfoodViewSet(ViewSet):
         integrador.criar_pedido_via_webhook(request.data)
 
         return Response(status=status.HTTP_202_ACCEPTED)
+
+    @action(methods=["post"], detail=False)
+    def importar_produtos_ifood(self, request):
+        client_id = Configuracao.get_configuracao("WCM_CLIENT_ID_IFOOD")
+        client_secret = Configuracao.get_configuracao("WCM_CLIENT_SECRET_IFOOD")
+
+        if not client_id or not client_secret:
+            return Response({"mensagem": "As credenciais do iFood não foram configuradas"}, status=status.HTTP_400_BAD_REQUEST)
+
+        integrador = ImportarProdutosIfood(client_id, client_secret, merchant="")
+
+        response = integrador.importar_produtos()
+
+        return Response(response, status=status.HTTP_202_ACCEPTED)
