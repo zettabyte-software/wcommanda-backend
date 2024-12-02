87e70e13be92e4ffbe556b7815f37530a73378d3
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2024-12-02 16:19:09 -0300

    tests: finalização dos testes das acoes do comanda item

--- apps/comandas/tests/test_api.py
+++ apps/comandas/tests/test_api.py
@@ -2,12 +2,24 @@ from rest_framework import status
 
 import pytest
 
-from apps.comandas.models import Comanda, ComandaItem, StatusComandaChoices
+from apps.comandas.models import Comanda, ComandaItem, StatusComandaChoices, StatusComandaItemChoices
 
 
 @pytest.fixture
-def comanda(db, usuario, mesa):
+def comanda(usuario, mesa):
     return Comanda.objects.create(cm_cliente="Cliente Teste", cm_mesa=mesa, cm_garcom=usuario)
+
+
+@pytest.fixture
+def comanda_item(produto, comanda):
+    return ComandaItem.objects.create(
+        ct_comanda=comanda,
+        ct_produto=produto,
+        ct_preco_unitario_produto=produto.pr_preco,
+    )
+
+
+# comanda
 
 
 @pytest.mark.django_db
@@ -20,7 +32,7 @@ def test_criar_comanda(api_client, usuario, mesa):
 
 
 @pytest.mark.django_db
-def test_finalizar_comanda(api_client, usuario, comanda):
+def test_finalizar_comanda(api_client, comanda):
     url = f"/api/v1/comandas/{comanda.id}/finalizar/"
     dados = {"cm_forma_pagamento": 1}
     response = api_client.post(url, dados, format="json")
@@ -30,7 +42,7 @@ def test_finalizar_comanda(api_client, usuario, comanda):
 
 
 @pytest.mark.django_db
-def test_cancelar_comanda(api_client, usuario, comanda):
+def test_cancelar_comanda(api_client, comanda):
     url = f"/api/v1/comandas/{comanda.id}/cancelar/"
     dados = {"cm_motivo": "O cliente não quis pagar previamente"}
     response = api_client.post(url, dados, format="json")
@@ -40,7 +52,7 @@ def test_cancelar_comanda(api_client, usuario, comanda):
 
 
 @pytest.mark.django_db
-def test_visualizar_comanda(api_client, usuario, comanda, produto):
+def test_visualizar_comanda(api_client, comanda, produto):
     comanda_item = ComandaItem.objects.create(
         ct_comanda=comanda,
         ct_produto=produto,
@@ -58,3 +70,46 @@ def test_visualizar_comanda(api_client, usuario, comanda, produto):
     assert response.data["cm_valor_total"] == comanda_item.ct_preco_unitario_produto
     assert response.data["cm_itens"][0]["ct_produto"] == produto.id
     assert response.data["cm_itens"][0]["ct_preco_unitario_produto"] == produto.pr_preco
+
+
+# comandas itens
+
+
+@pytest.mark.django_db
+def test_criar_comanda_item(api_client, comanda, produto):
+    url = "/api/v1/itens_comanda/"
+    dados = {
+        "ct_comanda": comanda.id,
+        "ct_produto": produto.id,
+        "ct_preco_unitario_produto": produto.pr_preco,
+    }
+    response = api_client.post(url, dados, format="json")
+    assert response.status_code == status.HTTP_201_CREATED
+    assert response.data["ct_status"] == StatusComandaItemChoices.ABERTO
+
+
+@pytest.mark.django_db
+def test_finalizar_comanda_item(api_client, comanda_item):
+    url = f"/api/v1/itens_comanda/{comanda_item.id}/finalizar/"
+    response = api_client.post(url, None, format="json")
+    assert response.status_code == status.HTTP_200_OK
+    comanda_item.refresh_from_db()
+    assert comanda_item.ct_status == StatusComandaItemChoices.PRONTO
+
+
+@pytest.mark.django_db
+def test_entregar_comanda_item(api_client, comanda_item):
+    url = f"/api/v1/itens_comanda/{comanda_item.id}/entregar/"
+    response = api_client.post(url, None, format="json")
+    assert response.status_code == status.HTTP_200_OK
+    comanda_item.refresh_from_db()
+    assert comanda_item.ct_status == StatusComandaItemChoices.ENTREGUE
+
+
+@pytest.mark.django_db
+def test_cancelar_comanda_item(api_client, comanda_item):
+    url = f"/api/v1/itens_comanda/{comanda_item.id}/cancelar/"
+    response = api_client.post(url, None, format="json")
+    assert response.status_code == status.HTTP_200_OK
+    comanda_item.refresh_from_db()
+    assert comanda_item.ct_status == StatusComandaItemChoices.CANCELADO
