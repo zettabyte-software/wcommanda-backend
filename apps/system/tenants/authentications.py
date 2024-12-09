b158196e34bc09e8947c8e06d0e5b4a75b272da5
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2024-12-09 15:22:36 -0300

    style: isort formatter

--- apps/system/tenants/authentications.py
+++ apps/system/tenants/authentications.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 
 from rest_framework.exceptions import AuthenticationFailed
+
 from rest_framework_simplejwt.authentication import JWTAuthentication as SimpleJWTAuthentication
 from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from rest_framework_simplejwt.settings import api_settings
