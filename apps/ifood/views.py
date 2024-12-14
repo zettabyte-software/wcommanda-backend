e2e229a8f8da5944bec6e81833d782e8443f4e58
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2024-12-13 23:36:30 -0300

    feat: endpoint para importação das categorias

--- apps/ifood/views.py
+++ apps/ifood/views.py
@@ -5,6 +5,7 @@ from rest_framework.viewsets import ViewSet
 
 from apps.system.conf.models import Configuracao
 
+from .integradores.categorias import IntegradorCategoriasIfood
 from .integrators import ImportarProdutosIfood, IntegradorPedidosIfood
 
 
@@ -34,5 +35,20 @@ class IfoodViewSet(ViewSet):
         integrador = ImportarProdutosIfood(client_id, client_secret, merchant="")
 
         response = integrador.importar_produtos()
+
+        return Response(response, status=status.HTTP_202_ACCEPTED)
+
+
+    @action(methods=["post"], detail=False)
+    def importar_categorias(self, request):
+        client_id = Configuracao.get_configuracao("WCM_CLIENT_ID_IFOOD")
+        client_secret = Configuracao.get_configuracao("WCM_CLIENT_SECRET_IFOOD")
+
+        if not client_id or not client_secret:
+            return Response({"mensagem": "As credenciais do iFood não foram configuradas"}, status=status.HTTP_400_BAD_REQUEST)
+
+        integrador = IntegradorCategoriasIfood(client_id, client_secret, merchant="85330304-7ee6-4182-b40d-6c65cca35a65", catalog_id="23786d4c-46da-49fe-8f77-234951725fd4")
+
+        response = integrador.importar_categorias()
 
         return Response(response, status=status.HTTP_202_ACCEPTED)
