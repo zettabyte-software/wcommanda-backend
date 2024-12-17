3c71fc171be3edf3dcba322f6250845f41d24be2
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2024-12-16 21:23:58 -0300

    fix: arquivo apps do app filial

--- apps/filas/apps.py
+++ apps/filas/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class FilasConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'filas'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.filas"
