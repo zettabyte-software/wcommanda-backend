03512cf60ca096685ab0091a1196280254269a47
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2024-12-20 18:03:20 -0300

    chore: função que não está validada

--- apps/system/base/serializers.py
+++ apps/system/base/serializers.py
@@ -21,14 +21,14 @@ class BaseModelSerializer(serializers.ModelSerializer):
     def get_field_verbose_name(self, field_name):
         return self.Meta.model._meta.get_field(field_name).verbose_name  # type: ignore
 
-    def run_validation(self, data=serializers.empty):
-        try:
-            return super().run_validation(data)
-        except serializers.ValidationError as e:
-            errors = e.detail
-            new_errors = {}
-            for field_name, field_errors in errors.items():
-                verbose_name = self.get_field_verbose_name(field_name)
-                new_errors[verbose_name] = field_errors
+    # def run_validation(self, data=serializers.empty):
+    #     try:
+    #         return super().run_validation(data)
+    #     except serializers.ValidationError as e:
+    #         errors = e.detail
+    #         new_errors = {}
+    #         for field_name, field_errors in errors.items():
+    #             verbose_name = self.get_field_verbose_name(field_name)
+    #             new_errors[verbose_name] = field_errors
 
-            raise serializers.ValidationError(new_errors)
+    #         raise serializers.ValidationError(new_errors)
