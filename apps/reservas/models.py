43adb8a7be17b23141664793847414dc22ecdd36
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2024-12-23 10:18:25 -0300

    fix: tornando o campo de cliente fidelidade não obrigatório

--- apps/reservas/models.py
+++ apps/reservas/models.py
@@ -8,6 +8,7 @@ from django_multitenant.fields import TenantForeignKey
 from apps.system.base.models import Base
 
 
+# TODO fazer uma background task para enviar um SMS lembrando a pessoa da reserva
 class Reserva(Base):
     rs_codigo = models.UUIDField(_("código da reserva"), default=uuid.uuid4)
     rs_mesa = TenantForeignKey(
@@ -17,17 +18,20 @@ class Reserva(Base):
         verbose_name=_("Mesa"),
     )
     rs_cliente = models.CharField(_("cliente"), max_length=80)
+    rs_celular_cliente = models.CharField(_("cliente"), max_length=11, blank=True, default="")
     rs_cliente_fidelidade = TenantForeignKey(
         "clientes.Cliente",
+        verbose_name=_("cliente fidelidade"),
         related_name="reservas",
         on_delete=models.PROTECT,
-        verbose_name=_("Cliente"),
+        null=True
     )
     rs_data = models.DateField(_("data"))
     rs_hora = models.TimeField(_("hora"))
     rs_quantidade_pessoas = models.PositiveIntegerField(_("pessoas"), default=2)
     rs_observacao = models.TextField(_("observação"), blank=True)
     rs_hora_limite = models.TimeField(_("hora limite de chegada"), null=True)
+    # rs_minutos_envio_sms = models.TimeField(_(""), null=True)  # enviar um sms x minutos antes lembrando a reserva
 
     class Meta:
         db_table = "reserva"
