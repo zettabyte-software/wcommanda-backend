8ae0fedb6f925d99b3e0c6f536a9f7d3670c27cc
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2024-12-23 09:20:33 -0300

    del(chore):

--- apps/system/base/serializers.py
+++ apps/system/base/serializers.py
@@ -2,8 +2,6 @@ from rest_framework import serializers
 
 
 class BaseModelSerializer(serializers.ModelSerializer):
-    # owner = OnwerSerializer(read_only=True)
-
     def __init__(self, instance=None, data=serializers.empty, **kwargs):
         exclude_fields = kwargs.pop("exclude_fields", None)
 
@@ -21,15 +19,3 @@ class BaseModelSerializer(serializers.ModelSerializer):
 
     def get_field_verbose_name(self, field_name):
         return self.Meta.model._meta.get_field(field_name).verbose_name  # type: ignore
-
-    # def run_validation(self, data=serializers.empty):
-    #     try:
-    #         return super().run_validation(data)
-    #     except serializers.ValidationError as e:
-    #         errors = e.detail
-    #         new_errors = {}
-    #         for field_name, field_errors in errors.items():
-    #             verbose_name = self.get_field_verbose_name(field_name)
-    #             new_errors[verbose_name] = field_errors
-
-    #         raise serializers.ValidationError(new_errors)
