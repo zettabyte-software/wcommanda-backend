f83ee5cce292f967092a50e1bfa7d6bd6a27fa28
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-01-22 09:20:36 -0300

    refactor: inicio da refatoração da função de vendas por periodo

--- apps/vendas/dashboards.py
+++ apps/vendas/dashboards.py
@@ -1,44 +1,54 @@
-from django.db.models import Sum
+from collections import defaultdict
 
-from apps.produtos.models import Produto
-from utils.date import gerar_dias_entre_2_datas
+from django.db.models import F, Sum
+from django.db.models.functions import TruncDate
 
 from .models import VendaItem
 
 
 def vendas_por_periodo(data_inicio, data_fim):
-    produtos = Produto.objects.all().only("id", "pr_nome")
-    vendas = []
-    datas = gerar_dias_entre_2_datas(data_inicio, data_fim)
-    for data in datas:
-        informacoes_vendas = []
-        valor_total_vendido_dia = VendaItem.objects.filter(data_criacao=data).aggregate(total=Sum("vd_valor_total"))["total"]
-        quantidade_total_vendido_dia = VendaItem.objects.filter(data_criacao=data).aggregate(total=Sum("vd_quantidade"))["total"]
+    vendas_agregadas = (
+        VendaItem.objects.filter(data_criacao__range=[data_inicio, data_fim])
+        .annotate(data=TruncDate("data_criacao"), nome_produto=F("vd_produto__pr_nome"))
+        .values("data", "nome_produto")
+        .annotate(total_faturado=Sum("vd_quantidade"), total_vendido=Sum("vd_valor_total"))
+        .order_by("data", "nome_produto")
+    )
 
-        for produto in produtos:
-            itens_vendidos = VendaItem.objects.filter(
-                vd_produto=produto,
-                data_criacao=data,
-            )
+    totais_diarios = (
+        VendaItem.objects.filter(data_criacao__range=[data_inicio, data_fim])
+        .annotate(data=TruncDate("data_criacao"))
+        .values("data")
+        .annotate(valor_total_vendido_dia=Sum("vd_valor_total"), quantidade_total_vendido_dia=Sum("vd_quantidade"))
+        .order_by("data")
+    )
 
-            quantidade_total = itens_vendidos.aggregate(total=Sum("vd_quantidade"))["total"]
-            valor_total = itens_vendidos.aggregate(total=Sum("vd_valor_total"))["total"]
+    totais_por_dia = {
+        str(total["data"]): {
+            "valor_total_vendido_dia": total["valor_total_vendido_dia"] or 0,
+            "quantidade_total_vendido_dia": total["quantidade_total_vendido_dia"] or 0,
+        }
+        for total in totais_diarios
+    }
 
-            informacoes_vendas.append(
-                {
-                    "produto": produto.pr_nome,
-                    "total_faturado": quantidade_total or 0,
-                    "total_vendido": valor_total or 0,
-                }
-            )
+    vendas_por_dia = defaultdict(
+        lambda: {"data": "", "vendas": [], "valor_total_vendido_dia": 0, "quantidade_total_vendido_dia": 0}
+    )
 
-        vendas.append(
+    for venda in vendas_agregadas:
+        data_str = str(venda["data"])
+        vendas_por_dia[data_str]["data"] = data_str
+        vendas_por_dia[data_str]["vendas"].append(
             {
-                "data": data.strftime("%Y-%m-%d"),
-                "vendas": informacoes_vendas,
-                "valor_total_vendido_dia": valor_total_vendido_dia or 0,
-                "quantidade_total_vendido_dia": quantidade_total_vendido_dia or 0,
+                "produto": venda["nome_produto"],
+                "total_faturado": venda["total_faturado"] or 0,
+                "total_vendido": venda["total_vendido"] or 0,
             }
         )
+        vendas_por_dia[data_str].update(
+            totais_por_dia.get(data_str, {"valor_total_vendido_dia": 0, "quantidade_total_vendido_dia": 0})
+        )
+
+    vendas = [vendas_por_dia[str(data)] for data in sorted(vendas_por_dia.keys())]
 
     return vendas
