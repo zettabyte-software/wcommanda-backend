dfcc1aeb6a29fec6b9b0004d602e02786dd9c74c
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-01-24 17:04:55 -0300

    feat: refatoração da função create_dns

--- lib/cloudflare/dns.py
+++ lib/cloudflare/dns.py
@@ -2,8 +2,11 @@ import httpx
 
 from utils.env import get_env_var
 
+_WCOMMANDA_ZONE_ID = get_env_var("WCOMMANDA_ZONE_ID")
+_WCOMMANDA_IP_ADDRESS = get_env_var("WCOMMANDA_SERVER_IP_ADDRESS")
+
 _CLOUDFLARE_API_URL = "https://api.cloudflare.com/client/v4/"
-_CLOUDFLARE_API_TOKEN = "Bearer {}".format(get_env_var("CLOUDFLARE_API_TOKEN"))
+_CLOUDFLARE_API_TOKEN = f"Bearer {get_env_var('CLOUDFLARE_API_TOKEN')}"
 _CLOUDFLARE_API_HEADERS = {
     "Authorization": _CLOUDFLARE_API_TOKEN,
     "Content-Type": "application/json",
@@ -22,8 +25,15 @@ def get_dns_records(zone_id: str):
     return response.json()
 
 
-def create_dns_record(zone_id: str, data: dict):
-    url = f"zones/{zone_id}/dns_records"
+def create_dns_record(subdomain: str):
+    url = f"zones/{_WCOMMANDA_ZONE_ID}/dns_records"
+    data = {
+        "type": "A",
+        "name": subdomain,
+        "content": _WCOMMANDA_IP_ADDRESS,
+        "ttl": 1,
+        "proxied": True,
+    }
     response = cloudlfare_client.post(url, json=data)
     return response.json()
 
