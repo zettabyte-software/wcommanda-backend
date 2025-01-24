b3f4ad9539ccba575398ac45ac8162b4bc2b295e
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-01-24 16:59:08 -0300

    feat: novas env

--- utils/env.py
+++ utils/env.py
@@ -3,31 +3,52 @@ import os
 from typing import Literal
 
 EnviromentVar = Literal[
+    # api
     "DJANGO_SECRET_KEY",
     "DJANGO_DEBUG",
     "DJANGO_MODE",
     "DJANGO_EXECUTION_MODE",
     "DJANGO_LOG_LEVEL",
+
     "DATABASE_NAME",
     "DATABASE_USER",
     "DATABASE_PASSWORD",
     "DATABASE_HOST",
     "DATABASE_PORT",
+
+    "REDIS_HOST",
+    "REDIS_PORT",
+
+    "EMAIL_PASSWORD",
+
     "RABBITMQ_CONNECTION_STRING",
-    "CLOUDFLARE_API_TOKEN",
+
+    # back blaze
     "BACKBLAZE_APPLICATION_ID",
     "BACKBLAZE_APPLICATION_KEY",
     "BACKBLAZE_BUCKET_NAME",
-    "REDIS_HOST",
-    "REDIS_PORT",
-    "EMAIL_PASSWORD",
+
+    # ifood
     "IFOOD_CLIENT_ID",
     "IFOOD_CLIENT_SECRET",
+
+    # sentry
     "SENTRY_DSN",
+
+    # stripe
     "STRIPE_SECRET_KEY",
+
+    # twilio
     "TWILLIO_ACCOUNT_SID",
     "TWILLIO_AUTH_TOKEN",
     "TWILLIO_PHONE_NUMBER",
+
+    # cloudflare
+    "CLOUDFLARE_API_TOKEN",
+
+    # wcommanda
+    "WCOMMANDA_SERVER_IP_ADDRESS",
+    "WCOMMANDA_ZONE_ID",
 ]
 
 
