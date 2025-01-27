3de6a5ac9c09b23a45d301e1d2aa32e341fef81f
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-01-27 17:31:20 -0300

    feat: criação do admin para os planos e assinaturas

--- apps/system/assinaturas/admin.py
+++ apps/system/assinaturas/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Assinatura, Plano
+
+
+@admin.register(Assinatura)
+class AssinaturaAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "ss_nome",
+        "ss_subdominio",
+        "ss_cloudflare_id",
+        "ss_codigo_licenca",
+        "ss_status",
+        "ss_integracoes_ifood",
+    )
+    list_filter = ("ss_status",)
+    search_fields = (
+        "ss_nome",
+        "ss_subdominio",
+        "ss_cloudflare_id",
+        "ss_codigo_licenca",
+    )
+    ordering = ("-id",)
+    list_per_page = 25
+
+
+@admin.register(Plano)
+class PlanoAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "pl_nome",
+        "pl_tier",
+        "pl_assinatura",
+        "pl_numero_usuarios",
+        "pl_limite_integracoes_ifood",
+        "pl_valor_mensalidade",
+        "pl_observacao",
+    )
+    list_filter = (
+        "pl_tier",
+        "ativo",
+        "pl_assinatura",
+    )
+    search_fields = (
+        "pl_nome",
+        "pl_observacao",
+        "pl_assinatura__ss_nome",
+    )
+    ordering = ("-id",)
+    list_per_page = 25
