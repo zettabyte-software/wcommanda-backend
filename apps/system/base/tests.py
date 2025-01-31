ba3db2379aa70a691eae3889bc9961ca963cd76c
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-01-31 16:21:52 -0300

    feat: reinicio do modulo de testes base

--- apps/system/base/tests.py
+++ apps/system/base/tests.py
@@ -1,10 +1,3 @@
-from rest_framework.test import APITestCase
+from django.test import TestCase
 
-from django_multitenant.utils import set_current_tenant
-
-from apps.system.tenants.models import Ambiente
-
-
-class BaseTestCase(APITestCase):
-    def setUp(self):
-        set_current_tenant(Ambiente.objects.create(mb_nome="Teste", mb_subdominio="teste"))
+# Create your tests here.
