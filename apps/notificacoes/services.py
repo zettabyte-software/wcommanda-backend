c5e08432170a77afe8ee6d13866af9942e113caf
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-02-03 11:11:16 -0300

    feat: util para envio das notificações

--- apps/notificacoes/services.py
+++ apps/notificacoes/services.py
@@ -1,2 +1,35 @@
-def enviar_notificao_todos_os_usuarios():
-    pass
+import logging
+
+from apps.system.assinaturas.models import Assinatura
+from apps.users.models import Usuario
+
+from .models import Notificacao
+
+logger = logging.getLogger(__name__)
+
+
+def enviar_notificao_todos_os_usuarios(titulo: str, mensagem: str, assinatura: Assinatura | None = None):
+    usuarios = Usuario.objects.none()
+    if assinatura is None:
+        usuarios = Usuario.all_objects.all()
+    else:
+        usuarios = Usuario.objects.all()
+
+    notificacoes = []
+    for usuario in usuarios:
+        notificacao = Notificacao(
+            nt_titulo=titulo,
+            nt_mensagem=mensagem,
+            nt_usuario=usuario,
+        )
+        notificacoes.append(notificacao)
+
+    return Notificacao.objects.bulk_create(notificacoes)
+
+
+def enviar_notificao_usuario(titulo: str, mensagem: str, usuario: Usuario):
+    return Notificacao.objects.create(
+        nt_titulo=titulo,
+        nt_mensagem=mensagem,
+        nt_usuario=usuario,
+    )
