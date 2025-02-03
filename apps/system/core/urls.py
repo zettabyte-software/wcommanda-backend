3fd2f4f3e6510660d822df31c1fcaa5dd2b581ad
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-02-03 13:06:23 -0300

    dev: incluindo uma view com o status 418

--- apps/system/core/urls.py
+++ apps/system/core/urls.py
@@ -2,7 +2,10 @@ from django.urls import include, path
 
 from rest_framework.routers import DefaultRouter
 
+from .views import ImTeapootViewSet
+
 router_v1 = DefaultRouter()
+router_v1.register("i_am_a_teapoot", ImTeapootViewSet, "i_am_a_teapoot")
 
 urlpatterns = [
     path("v1/", include(router_v1.urls)),
--- apps/system/core/views.py
+++ apps/system/core/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
 
-# Create your views here.
+
+class ImTeapootViewSet(ViewSet):
+    authentication_classes = []
+    permission_classes = [AllowAny]
+
+    def list(self, request):
+        return Response(status=status.HTTP_418_IM_A_TEAPOT)
+
+    def retrieve(self, request):
+        return Response(status=status.HTTP_418_IM_A_TEAPOT)
+
+    def create(self, request):
+        return Response(status=status.HTTP_418_IM_A_TEAPOT)
+
+    def partial_update(self, request):
+        return Response(status=status.HTTP_418_IM_A_TEAPOT)
+
+    def update(self, request):
+        return Response(status=status.HTTP_418_IM_A_TEAPOT)
+
+    def destroy(self, request):
+        return Response(status=status.HTTP_418_IM_A_TEAPOT)
+
