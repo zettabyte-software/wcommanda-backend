5f06ac2d889a135149731826aaeaf6d62b01c5f2
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-02-03 11:18:19 -0300

    style: ruff

--- apps/ifood/limiter.py
+++ apps/ifood/limiter.py
@@ -12,7 +12,7 @@ class LimitadorIntegracaoPedidosIfood:
     def __init__(self):
         tenant = get_current_tenant()
         self.tenant = tenant
-        self.limite = 5  # tenant.ss_plano.pl_numero_integracoes_ifood  # type: ignore
+        self.limite = tenant.ss_plano.pl_numero_integracoes_ifood  # type: ignore
 
     @property
     def total_pedidos_mes(self):
@@ -22,5 +22,4 @@ class LimitadorIntegracaoPedidosIfood:
 
     @property
     def atingiu_limite_pedidos(self):
-        logger.info("Atingiu o limite: %s > %s (%s)", self.total_pedidos_mes, self.limite, self.total_pedidos_mes >= self.limite)
         return self.total_pedidos_mes > self.limite
