d07e4c31598c57d7f7ae4f9789149e1e987431a2
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-02-04 11:22:55 -0300

    feat: upload da foto direto para o back blaze

--- apps/produtos/models.py
+++ apps/produtos/models.py
@@ -1,12 +1,22 @@
+import uuid
+
 from django.core.files.uploadedfile import InMemoryUploadedFile
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 from django_multitenant.fields import TenantForeignKey
+from django_multitenant.utils import get_current_tenant
 
 from apps.comandas.models import StatusComandaItemChoices
 from apps.system.base.models import Base
+from lib.back_blaze.bucket import BackBlazeB2Handler
+from utils.env import get_env_var
+
+DEFAULT_BUCKET_PHOTO_PATH = '%s/produtos/%s/imgs/%s'
+"""Caminho padrão para a imagem dentro do bucket: \n
+<id_tenant>/produtos/<id_produto>/imgs/<nome_arquivo>
+"""
 
 
 class TiposChoices(models.IntegerChoices):
@@ -46,8 +56,8 @@ class Produto(Base):
         default=StatusComandaItemChoices.ABERTO,
     )
 
-    pr_imagem = models.URLField(_("foto"), blank=True, default="")
-    # pr_url_imagem = models.URLField(_("foto"), blank=True, default="")
+    pr_path_imagem = models.CharField(_("foto"), max_length=40, blank=True, default="")
+    pr_id_back_blaze = models.CharField(_("foto"), max_length=40, blank=True, default="")
 
     # restrições alimentares
     pr_vegano = models.BooleanField(_("vegano"), default=False)
@@ -66,7 +76,6 @@ class Produto(Base):
         related_name="produtos",
     )
 
-
     # porção da comida
     pr_serve_pessoas = models.PositiveSmallIntegerField(_("n° de pessoas que a comida serve"), validators=[MaxValueValidator(4)], null=True)
     pr_unidade = models.PositiveSmallIntegerField(_("tipo"), choices=UnidadesProdutoChoices.choices, null=True)
@@ -74,9 +83,39 @@ class Produto(Base):
 
     # pr_integra_ifood = models.BooleanField(_("integra com o ifood"), default=False)
 
+    @property
+    def pr_url_imagem(self):
+        if not self.pr_path_imagem:
+            return None
+
+        bucket = get_env_var("BACKBLAZE_BUCKET_NAME")
+        return f'https://f005.backblazeb2.com/file/{bucket}/{self.pr_path_imagem}'
+
     @classmethod
-    def upload(cls, produto: "Produto", image: InMemoryUploadedFile):
-        raise Exception
+    def upload(cls, produto: "Produto", arquivo: InMemoryUploadedFile, metadata="{}"):
+        handler = BackBlazeB2Handler()
+        assinatura = get_current_tenant()
+        nome_aleatorio_imagem = str(uuid.uuid4())
+
+        path = DEFAULT_BUCKET_PHOTO_PATH % (
+            assinatura.ss_codigo_licenca,  # type: ignore
+            produto.pk,
+            nome_aleatorio_imagem,
+        )
+
+        file_version = handler.upload(arquivo.read(), path, metadata)
+
+        produto.pr_path_imagem = path
+        produto.pr_id_back_blaze = file_version.id_
+        produto.save()
+
+    @classmethod
+    def remover_foto(cls, produto: "Produto"):
+        handler = BackBlazeB2Handler()
+        produto.pr_path_imagem = ""
+        produto.pr_id_back_blaze = ""
+        produto.save()
+        return handler.destroy(produto.pr_id_back_blaze, produto.pr_path_imagem)
 
     class Meta:
         db_table = "produto"
--- apps/produtos/serializers.py
+++ apps/produtos/serializers.py
@@ -1,3 +1,5 @@
+from rest_framework import serializers
+
 from apps.system.base.serializers import BaseModelSerializer
 
 from .models import CategoriaProduto, ComplementoProduto, GrupoComplementoProduto, Produto
@@ -11,6 +13,10 @@ class CategoriaProdutoSerializer(BaseModelSerializer):
 
 class ProdutoVisualizacaoSerializer(BaseModelSerializer):
     pr_categoria = CategoriaProdutoSerializer()
+    pr_url_imagem = serializers.SerializerMethodField(read_only=True)
+
+    def get_pr_url_imagem(self, obj):
+        return obj.pr_url_imagem
 
     class Meta:
         model = Produto
@@ -18,11 +24,31 @@ class ProdutoVisualizacaoSerializer(BaseModelSerializer):
 
 
 class ProdutoAlteracaoSerializer(BaseModelSerializer):
+    pr_imagem = serializers.ImageField(write_only=True, required=False)
+
+    def create(self, validated_data):
+        imagem = validated_data.pop("pr_imagem", None)
+        produto = Produto.objects.create(**validated_data)
+
+        if imagem:
+            Produto.upload(produto, imagem)
+
+        return produto
+
     def update(self, instance, validated_data):
-        if "pr_imagem" in validated_data and validated_data["pr_imagem"] is None:
-            instance.pr_imagem.delete(save=False)
-            instance.pr_imagem = None
-        return super().update(instance, validated_data)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        if 'pr_imagem' in self.initial_data:  # type: ignore
+            imagem = validated_data.get('pr_imagem', None)
+            if instance.pr_id_back_blaze and instance.pr_path_imagem:
+                Produto.remover_foto(instance)
+
+            if imagem is not None:
+                Produto.upload(instance, imagem)
+
+        return instance
 
     class Meta:
         model = Produto
