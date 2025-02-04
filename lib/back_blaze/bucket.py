91c90c06fe5250f6041af7be9f6a65c2b6fb7834
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-02-04 11:21:25 -0300

    feat: singleton para perfomance + método de exclusão da foto

--- lib/back_blaze/bucket.py
+++ lib/back_blaze/bucket.py
@@ -2,10 +2,11 @@ from django.core.cache import cache
 
 from b2sdk.v2 import AuthInfoCache, B2Api, DoNothingProgressListener, InMemoryAccountInfo
 
+from apps.system.core.classes import SingletonMeta
 from utils.env import get_env_var
 
 
-class BackBlazeB2Handler:
+class BackBlazeB2Handler(SingletonMeta):
     def __init__(self):
         application_key_id = get_env_var("BACKBLAZE_APPLICATION_ID")
         application_key = get_env_var("BACKBLAZE_APPLICATION_KEY")
@@ -15,9 +16,9 @@ class BackBlazeB2Handler:
 
         self.b2_api = B2Api(info, cache=AuthInfoCache(info))
         self.b2_api.authorize_account("production", application_key_id, application_key)
-        self.bucket = self.b2_api.get_bucket_by_name(bucket_name)
+        self.bucket = self.b2_api.get_bucket_by_name(bucket_name)  # type: ignore
 
-    def upload(self, bytes_arquivo, path, metadata=None):
+    def upload(self, bytes_arquivo: bytes, path: str, metadata=None):
         if metadata is None:
             metadata = {}
 
@@ -29,7 +30,7 @@ class BackBlazeB2Handler:
 
         return back_blaze_file
 
-    def download(self, file_id):
+    def download(self, file_id: str):
         cache_image = cache.get(file_id)
         if cache_image:
             return cache_image
@@ -43,3 +44,7 @@ class BackBlazeB2Handler:
         cache.set(file_id, downloaded_file, 60 * 60 * 24)
 
         return downloaded_file
+
+    def destroy(self, file_id: str, nome_arquivo: str):
+        response = self.b2_api.delete_file_version(file_id, nome_arquivo)
+        return response.as_dict()
