923543ef37a04a9a3b21f98cd08873f682b5b312
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-02-13 15:40:53 -0300

    feat: adição de checkagem das envs para produção

--- apps/system/core/apps.py
+++ apps/system/core/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 
+from common.internal.checks.api.envs import check_required_env_vars  # noqa: F401
+
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
