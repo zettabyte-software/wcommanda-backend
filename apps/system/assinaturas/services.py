c6c656b01ed2551139e8fb6f0020bee0d07632f6
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-02-14 17:43:44 -0300

    feat: adicionar suporte para criação de assinatura com plano baseado em tier

--- apps/system/assinaturas/services.py
+++ apps/system/assinaturas/services.py
@@ -3,15 +3,16 @@ from django.contrib.auth.hashers import make_password
 
 from django_multitenant.utils import set_current_tenant
 
-from apps.system.assinaturas.models import Assinatura
+from apps.system.assinaturas.models import Assinatura, Plano, StatusChoices, TierChoices
 from apps.system.core.records import DefaultRecordsManger
 from apps.users.models import Usuario
-from lib.cloudflare.dns import create_dns_record
+from lib.cloudflare.dns import criar_subdominio
 
 
+# TODO criar testes para criação de assinatura
 class InicializadorAssinatura:
-    def criar_assinatura(self, dados_ambient, dados_usuario):
-        self._criar_assinatura(dados_ambient["mb_nome"], dados_ambient["mb_subdominio"])
+    def criar_assinatura(self, dados_ambiente: dict, dados_usuario: dict, tier: TierChoices):
+        self._criar_assinatura(dados_ambiente["mb_nome"], dados_ambiente["mb_subdominio"], tier)
         self.criar_usuario_root(
             dados_usuario["first_name"],
             dados_usuario["last_name"],
@@ -20,28 +21,20 @@ class InicializadorAssinatura:
         )
         self.popular_registros_padroes()
 
-    def _criar_assinatura(self, nome: str, subdominio: str):
-        raise Exception
+    def _criar_assinatura(self, nome: str, subdominio: str, tier: TierChoices):
+        assinatura = Assinatura.objects.create(
+            ss_subdominio=subdominio,
+            ss_nome=nome,
+            ss_cloudflare_id="dev-id",
+            ss_status=StatusChoices.ULTRA,
+        )
 
-        # assinatura = Assinatura.objects.create(
-        #     ss_subdominio=subdominio,
-        #     ss_nome=nome,
-        #     ss_cloudflare_id="dev-id",
-        #     ss_status=StatusChoices.ULTRA,
-        # )
-        # plano = Plano.objects.create(
-        #     pl_nome="Dev",
-        #     pl_tier=TierChoices.TIER_4,
-        #     pl_numero_usuarios=100,
-        #     pl_limite_integracoes_ifood=99999,
-        #     pl_valor_mensalidade=0,
-        #     pl_observacao="",
-        #     assinatura=assinatura,
-        # )
+        Plano.criar_plano(tier)
 
-        # set_current_tenant(ambiente)
-        # if settings.IN_PRODUCTION:
-        #     create_dns_record(ambiente.mb_subdominio)
+        set_current_tenant(assinatura)
+
+        if settings.IN_PRODUCTION:
+            criar_subdominio(assinatura.ss_subdominio)
 
     def criar_usuario_root(self, nome: str, sobrenome: str, email: str, senha: str):
         usuario = Usuario()
