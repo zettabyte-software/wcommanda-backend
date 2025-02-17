1c001b04cfaa600c51a5f260f6bcf7f376ba3da8
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-02-17 10:36:26 -0300

    feat: refatorar verificação de variáveis de ambiente para usar constante centralizada

--- common/internal/checks/api/envs.py
+++ common/internal/checks/api/envs.py
@@ -2,49 +2,20 @@ import os
 
 from django.core.checks import Error, Tags, register
 
+from utils.env import ENVS
 
-@register(Tags.compatibility, deploy=True)
+
+@register(Tags.compatibility)
 def check_required_env_vars(app_configs, **kwargs):
     """Verifica se todas as variáveis de ambiente necessárias estão definidas.
 
     Levanta ImproperlyConfigured se alguma variável obrigatória estiver faltando.
     """
 
-    envs  = {
-        "DJANGO_SECRET_KEY",
-        "DJANGO_DEBUG",
-        "DJANGO_MODE",
-        "DJANGO_EXECUTION_MODE",
-        "DJANGO_LOG_LEVEL",
-        "DATABASE_NAME",
-        "DATABASE_USER",
-        "DATABASE_PASSWORD",
-        "DATABASE_HOST",
-        "DATABASE_PORT",
-        "REDIS_HOST",
-        "REDIS_PORT",
-        "EMAIL_PASSWORD",
-        "RABBITMQ_CONNECTION_STRING",
-        "BACKBLAZE_APPLICATION_ID",
-        "BACKBLAZE_APPLICATION_KEY",
-        "BACKBLAZE_BUCKET_NAME",
-        "IFOOD_CLIENT_ID",
-        "IFOOD_CLIENT_SECRET",
-        "SENTRY_DSN",
-        "STRIPE_SECRET_KEY",
-        "STRIPE_WEBHOOK_SECRET",
-        "TWILLIO_ACCOUNT_SID",
-        "TWILLIO_AUTH_TOKEN",
-        "TWILLIO_PHONE_NUMBER",
-        "CLOUDFLARE_API_TOKEN",
-        "WCOMMANDA_SERVER_IP_ADDRESS",
-        "WCOMMANDA_ZONE_ID",
-    }
-
     missing = []
     msg = "%s ENV ESTÁ VAZIA"
 
-    for env in envs:
+    for env in ENVS:
         if env not in os.environ:
             missing.append(
                 Error(
--- utils/env.py
+++ utils/env.py
@@ -2,7 +2,7 @@ import ast
 import os
 from typing import Literal
 
-EnviromentVar = Literal[
+ENVS  = {
     # api
     "DJANGO_SECRET_KEY",
     "DJANGO_DEBUG",
@@ -28,9 +28,51 @@ EnviromentVar = Literal[
     "BACKBLAZE_APPLICATION_KEY",
     "BACKBLAZE_BUCKET_NAME",
 
-    # ifood
-    "IFOOD_CLIENT_ID",
-    "IFOOD_CLIENT_SECRET",
+    # sentry
+    "SENTRY_DSN",
+
+    # stripe
+    "STRIPE_SECRET_KEY",
+    "STRIPE_WEBHOOK_SECRET",
+
+    # twilio
+    "TWILLIO_ACCOUNT_SID",
+    "TWILLIO_AUTH_TOKEN",
+    "TWILLIO_PHONE_NUMBER",
+
+    # cloudflare
+    "CLOUDFLARE_API_TOKEN",
+
+    # wcommanda
+    "WCOMMANDA_SERVER_IP_ADDRESS",
+    "WCOMMANDA_ZONE_ID",
+}
+
+EnviromentVar = Literal[
+    # api
+    "DJANGO_SECRET_KEY",
+    "DJANGO_DEBUG",
+    "DJANGO_MODE",
+    "DJANGO_EXECUTION_MODE",
+    "DJANGO_LOG_LEVEL",
+
+    "DATABASE_NAME",
+    "DATABASE_USER",
+    "DATABASE_PASSWORD",
+    "DATABASE_HOST",
+    "DATABASE_PORT",
+
+    "REDIS_HOST",
+    "REDIS_PORT",
+
+    "EMAIL_PASSWORD",
+
+    "RABBITMQ_CONNECTION_STRING",
+
+    # back blaze
+    "BACKBLAZE_APPLICATION_ID",
+    "BACKBLAZE_APPLICATION_KEY",
+    "BACKBLAZE_BUCKET_NAME",
 
     # sentry
     "SENTRY_DSN",
