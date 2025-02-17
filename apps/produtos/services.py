20497e654289180ebf115d96933f2b15adeaa4ad
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-02-17 11:21:17 -0300

    style: ruff formatter

--- apps/produtos/services.py
+++ apps/produtos/services.py
@@ -7,6 +7,12 @@ from .models import Produto
 
 logger = logging.getLogger(__name__)
 
+
 def gerar_codigo_cardapio():
-    codigo = Produto.objects.annotate(codigo_num=Cast('pr_codigo_cardapio', IntegerField())).aggregate(codigo=Max('codigo_num'))['codigo'] or 0
+    codigo = (
+        Produto.objects.annotate(codigo_num=Cast("pr_codigo_cardapio", IntegerField())).aggregate(
+            codigo=Max("codigo_num")
+        )["codigo"]
+        or 0
+    )
     return int(codigo) + 1
