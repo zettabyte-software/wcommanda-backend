303c1323f3bd12bac542f8e8062f0a14815e3b02
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-04-21 13:53:22 +0000

    style: remoção de imports não utilizados

--- apps/garcons/views.py
+++ apps/garcons/views.py
@@ -2,13 +2,12 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from apps.system.base.views import BaseModelViewSet
-from apps.system.base.views import BaseModelViewSet
 
 from .models import StatusComissaoGarcomChoices
 from .serializers import (
     ComissaoGarcom,
-    ComissaoGarcomVisualizacaoSerializer,
     ComissaoGarcomAlteracaoSerializer,
+    ComissaoGarcomVisualizacaoSerializer,
 )
 
 
