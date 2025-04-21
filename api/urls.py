67caf5e730b56a7fcc271446dd58e1a5ba2adc8e
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-04-21 13:52:52 +0000

    feat: otimizando busca dos urlpatterns
    só tenta adicionar as urls, caso o app tenha o arquivo `urls.py`

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,5 @@
+import os
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -5,7 +7,11 @@ from django.urls import include, path
 
 from apps.system.core.handlers import custom_404_handler
 
-apps_urls = [path("api/", include(app + ".urls")) for app in settings.WCOMMANDA_APPS]
+apps_urls = [
+    path("", include(app + ".urls"))
+    for app in settings.WCOMMANDA_APPS
+    if os.path.exists(os.path.join(settings.BASE_DIR, app.replace(".", "/"), "urls.py"))
+]
 
 if settings.IN_PRODUCTION:
     handler404 = custom_404_handler
