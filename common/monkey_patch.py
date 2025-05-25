b90a40ac7ccf384b7e838d4e63204930b7f7c663
Davi Silva Rafacho <rafacho.s.davi@gmail.com>
2025-05-25 22:08:05 +0000

    fix: tradução de bool estava errada

--- common/monkey_patch.py
+++ common/monkey_patch.py
@@ -21,13 +21,13 @@ def float_to_value(self, value):
     return float(value)
 
 
-def bool_to_value(self, value):
-    if value is None:
-        return None
-    return ast.literal_eval(value)
+# def bool_to_value(self, value):
+#     if value is None:
+#         return None
+#     return ast.literal_eval(value)
 
 
 serpy.StrField.to_value = str_to_value
 serpy.IntField.to_value = int_to_value
 serpy.FloatField.to_value = float_to_value
-serpy.BoolField.to_value = bool_to_value
+# serpy.BoolField.to_value = bool_to_value
